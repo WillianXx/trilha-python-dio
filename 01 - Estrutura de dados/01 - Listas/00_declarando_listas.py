d95c4ad47c5602556b649adaf58d70ee81418c40
WillianXx <williansilva200405@gmail.com>
2026-01-27 21:29:35 -0300

    feat: add estruturas de dados module files

--- 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
+++ 01 - Estrutura de dados/01 - Listas/00_declarando_listas.py
@@ -12,3 +12,27 @@ print(numeros)
 
 carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
 print(carro)
+
+matriz = [
+    [1, 'a', 2],
+    ['b', 3, 4],
+    [5, 6, 'c']]
+print(matriz[0][0])
+print(matriz[-1][-1])
+
+lista = list('python')
+print(lista[2:])
+print(lista[:2])
+print(lista[1:4:2])
+print(lista[:])
+print(lista[::-1])
+
+carros = ["gol", "celta", "palio"]
+for indice, carro in enumerate(carros):
+    print(f"{indice}: {carro}")
+
+numeros = [1,  30, 21, 2, 9, 65, 34]
+pares = [n for n in numeros if n % 2 == 0]
+quadrado = [n**2 for n in numeros]
+print(pares)
+print(quadrado)
